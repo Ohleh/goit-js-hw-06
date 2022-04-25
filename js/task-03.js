2fae1eb1eda9a3ad338c46c66e46f5948a97facc
Ohleh <oleg.kolischuk@gmail.com>
2022-04-25 19:50:25 +0300

    tsk 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,45 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryJs = document.querySelector(".gallery");
+console.log(galleryJs);
+
+// const massDataImages = images.map((image) => image.url);
+//console.log(massDataImages[0]);
+// const massDataAlt = images.map((image) => image.alt);
+//console.log(massDataAlt[0]);
+
+for (const image of images) {
+  galleryJs.insertAdjacentHTML(
+    "beforeend",
+    `<li class="galleryListTaskTree"> <img src=${image.url} alt=${image.alt} width = 700px>  </li>`
+  );
+}
+
+//
+//
+//
+// const massDataImages = images
+//   .map((image) => `<li> <img src="${image.url}" alt="${image.alt}"</li>`)
+//   .join("");
+// console.log(massDataImages);
+// galleryJs.insertAdjacentHTML("afterend", massDataImages);
+//
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+// Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
