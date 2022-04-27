761047eb5d1fd549d8520993901b562c077006f4
Ohleh <oleg.kolischuk@gmail.com>
2022-04-27 19:51:25 +0300

    tsk 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let inputNumber = document.querySelector("input");
+const boxeDiv = document.querySelector("#boxes");
+//
+// Create_button
+const createElemButton = document.querySelector("[data-create]");
+function createBoxes() {
+  let counterDiv = inputNumber.value;
+  let iPx = 30;
+  for (let i = 0; i < inputNumber.value; i += 1) {
+    boxeDiv.insertAdjacentHTML(
+      "beforeend",
+      `<div style="width:${iPx}px;height:${iPx}px;border:0.5px solid #000; background-color:${getRandomHexColor()}; margin:10px "></div>`
+    );
+    iPx += 10;
+  }
+}
+createElemButton.addEventListener("click", createBoxes);
+//
+// Destroy_button
+const dectrElemButton = document.querySelector("[data-destroy]");
+function dectrBoxes() {
+  inputNumber.value = "";
+  boxeDiv.innerHTML = "";
+}
+dectrElemButton.addEventListener("click", dectrBoxes);
