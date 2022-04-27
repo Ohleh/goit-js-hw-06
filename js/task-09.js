f2f44276ac9db200e006ed75b18808c6f7eabc49
Ohleh <oleg.kolischuk@gmail.com>
2022-04-27 15:28:28 +0300

    tsk 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+const bodyHtml = document.querySelector("body");
+const chColorBtn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+chColorBtn.addEventListener("click", handleBtnClick);
+
+function handleBtnClick() {
+  let changeColor = getRandomHexColor();
+  spanColor.textContent = changeColor;
+  bodyHtml.style.backgroundColor = changeColor;
+  console.log(changeColor);
+}
